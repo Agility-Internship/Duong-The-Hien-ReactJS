9ba74a889def803b52efa617cd85eae10ac4da74
Dương Thế Hiển <duongthehien2001@gmail.com>
2023-07-03 13:59:01 +0700

    #8 Feat: Create buttons components

--- practice-one/src/App.jsx
+++ practice-one/src/App.jsx
@@ -13,11 +13,6 @@ function App() {
       <Button text="login" variant="primary" onClick={handleClick} />
       <Button text="Try For Free" variant="secondary" onClick={handleClick} />
       <Button text="Try For Free" variant="outline" rightIcon={<i className="fas fa-check" />} onClick={handleClick} />
-      <Link href="_blank" isExternal imageVariant="rounded">
-        Link
-      </Link>
-      <Card maxW="xl" name="Card with Image" size="xl" variant="bold" onChange={() => console.log('Button clicked')} hasImage />
-      <Card maxW="lg" name="Card without Image" size="lg" variant="semibold" onChange={() => console.log('Button clicked')} hasImage={false} />
     </div>
   );
 }
