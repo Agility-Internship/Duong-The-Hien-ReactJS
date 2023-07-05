2f5b7606fc3d10e1187ae99d094f45f2b8310439
Dương Thế Hiển <duongthehien2001@gmail.com>
2023-07-05 11:11:25 +0700

    #16 Fix: Fix comment

--- practice-one/src/Components/Section/Navbar.jsx
+++ practice-one/src/Components/Section/Navbar.jsx
@@ -1,45 +1,36 @@
 import React from 'react';
-import Button from '../Common/Buttons';
+import { Nav } from 'react-bootstrap';
+import Button from '../Common/Button';
 
 const Navbar = () => (
-  <div className=" bg-light">
-    <nav className="container mx-auto w-screen h-[80px]  ">
+  <div className="bg-light">
+    <nav className="container mx-auto w-screen h-[80px]">
       <div className="px-2 flex justify-between items-center w-full h-full">
         <div className="flex items-center gap-5">
           <a className="navbar-brand logo" href="#">
             <img src="src\assets\logo.png" alt="logo" />
           </a>
-          <ul className="hidden ps-20 gap-10  md:flex">
-            <li>
-              <a href="/Product" className="text-base hover:text-primary">
-                Product
-              </a>
-            </li>
-            <li>
-              <a href="/Contact" className="text-base hover:text-primary">
-                Pricing
-              </a>
-            </li>
-            <li>
-              <a href="/Company" className="text-base hover:text-primary">
-                Company
-              </a>
-            </li>
-            <li>
-              <a href="/Recourses" className="text-base hover:text-primary">
-                Recourses
-              </a>
-            </li>
-            <li>
-              <a href="/Contact" className="text-base hover:text-primary">
-                Contact
-              </a>
-            </li>
-          </ul>
+          <Nav className="hidden ps-20 gap-10 md:flex">
+            <Nav.Link href="#Product" className="text-base hover:text-primary">
+              Product
+            </Nav.Link>
+            <Nav.Link href="#Contact" className="text-base hover:text-primary">
+              Pricing
+            </Nav.Link>
+            <Nav.Link href="#Company" className="text-base hover:text-primary">
+              Company
+            </Nav.Link>
+            <Nav.Link href="#Recourses" className="text-base hover:text-primary">
+              Resources
+            </Nav.Link>
+            <Nav.Link href="#Contact" className="text-base hover:text-primary">
+              Contact
+            </Nav.Link>
+          </Nav>
         </div>
         <div className="hidden md:flex pr-4 gap-5">
-          <Button text="Log in" size="medium" variant="primary" />
-          <Button text="Try for Free" size="medium" variant="secondary" />
+          <Button text="Log in" variant="primary" />
+          <Button text="Try for Free" variant="secondary" />
         </div>
       </div>
     </nav>
