3dffd60dab801af760a6df40122f0c7768c1dd96
Dương Thế Hiển <duongthehien2001@gmail.com>
2023-07-06 10:26:38 +0700

    #19 Feat: Create productList-section

--- practice-one/src/App.jsx
+++ practice-one/src/App.jsx
@@ -3,11 +3,14 @@ import React from 'react';
 // Components
 import Navbar from './Components/Section/Navbar';
 import Header from './Components/Section/Header';
+import ListCard from './Components/Section/ProductList';
 
 const App = () => (
   <div>
     <Navbar />
     <Header />
+    <ListCard />
+
   </div>
 );
 
