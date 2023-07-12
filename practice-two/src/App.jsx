2efff8f40296ac7abc85ff635ce42ef4a0a3d8d5
Phat Truong <phattruong@phats-mbp.lan>
2023-07-12 14:19:41 +0700

    test

--- practice-two/src/App.jsx
+++ practice-two/src/App.jsx
@@ -2,9 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <h1 className="text-5xl font-bold underline">
-      Hello world!
-    </h1>
+    <h1 className="text-5xl font-bold underline">Hello world!</h1>
   );
 }
 
