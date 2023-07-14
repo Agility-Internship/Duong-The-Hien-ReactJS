25add973967eb36e5b5b765b01b7217ba162907a
Dương Thế Hiển <duongthehien2001@gmail.com>
2023-07-14 14:12:07 +0700

    #46 Fix: Fix comment

--- practice-two/src/App.jsx
+++ practice-two/src/App.jsx
@@ -9,26 +9,43 @@ const App = () => {
   return (
     <>
       <div className=" flex gap-6">
-        <Button variant="primary" leftIcon onClick={handlePrimaryButtonClick}>
+        <Button variant="primary" onClick={handlePrimaryButtonClick}>
           Filter
+          <img src="public\images\Filter-icon.png" alt="filter-icon" className="w-4" />
         </Button>
-        <Button variant="primary" rightIcon onClick={handlePrimaryButtonClick}>
+        <Button variant="primary" onClick={handlePrimaryButtonClick}>
           Manufacturer
+          <img src="public\images\down.png" alt="down-cion" className="w-4" />
         </Button>
-        <Button variant="primary" rightIcon onClick={handlePrimaryButtonClick}>
+        <Button variant="primary" onClick={handlePrimaryButtonClick}>
           Prices
         </Button>
       </div>
       <div className="mt-5 flex flex-wrap gap-3 ">
-        <Button variant="secondary" image="public/images/realme.png" />
-        <Button variant="secondary" image="public/images/realme.png" />
-        <Button variant="secondary" image="public/images/realme.png" />
-        <Button variant="secondary" image="public/images/realme.png" />
-        <Button variant="secondary" image="public/images/realme.png" />
-        <Button variant="secondary" image="public/images/realme.png" />
-        <Button variant="secondary" image="public/images/realme.png" />
-        <Button variant="secondary" image="public/images/realme.png" />
-        <Button variant="secondary" image="public/images/realme.png" />
+        <Button variant="secondary" size="small" onClick={handlePrimaryButtonClick}>
+          <img src="http://cdn.tgdd.vn/Brand/1/logo-xiaomi-220x48-5.png" alt="Button icon" className="w-full " />
+        </Button>
+        <Button variant="secondary" size="small" onClick={handlePrimaryButtonClick}>
+          <img src="http://cdn.tgdd.vn/Brand/1/logo-xiaomi-220x48-5.png" alt="Button icon" className="w-full" />
+        </Button>
+        <Button variant="secondary" size="small" onClick={handlePrimaryButtonClick}>
+          <img src="http://cdn.tgdd.vn/Brand/1/logo-xiaomi-220x48-5.png" alt="Button icon" className="w-full" />
+        </Button>
+        <Button variant="secondary" size="small" onClick={handlePrimaryButtonClick}>
+          <img src="http://cdn.tgdd.vn/Brand/1/logo-xiaomi-220x48-5.png" alt="Button icon" className="w-full" />
+        </Button>
+        <Button variant="secondary" size="small" onClick={handlePrimaryButtonClick}>
+          <img src="http://cdn.tgdd.vn/Brand/1/logo-xiaomi-220x48-5.png" alt="Button icon" className="w-full" />
+        </Button>
+        <Button variant="secondary" size="small" onClick={handlePrimaryButtonClick}>
+          <img src="http://cdn.tgdd.vn/Brand/1/logo-xiaomi-220x48-5.png" alt="Button icon" className="w-full" />
+        </Button>
+        <Button variant="secondary" size="small" onClick={handlePrimaryButtonClick}>
+          <img src="http://cdn.tgdd.vn/Brand/1/logo-xiaomi-220x48-5.png" alt="Button icon" className="w-full" />
+        </Button>
+        <Button variant="secondary" size="small" onClick={handlePrimaryButtonClick}>
+          <img src="http://cdn.tgdd.vn/Brand/1/logo-xiaomi-220x48-5.png" alt="Button icon" className="w-full" />
+        </Button>
       </div>
     </>
   );
--- practice-two/src/components/common/Button.jsx
+++ practice-two/src/components/common/Button.jsx
@@ -33,9 +33,6 @@ const colorClasses = {
  * @param children Text for the button.
  * @param variant Variant of the button. Can be one of 'primary', 'secondary'
  * @param size Size of the button. Can be one of 'small', 'medium', or 'large'.
- * @param rightIcon Path to the right icon image.
- * @param leftIcon Path to the left icon image.
- * @param image Path to the button image.
  * @param type Type of the button. Can be one of 'button', 'submit', or 'reset'.
  * @param color Color of the button. Can be one of 'green', 'blue', 'red', 'gray'.
  * @param onClick Click event handler function for the button.
@@ -45,17 +42,11 @@ const Button = ({
   children,
   variant = 'primary',
   size = 'medium',
-  rightIcon,
-  leftIcon,
-  image,
   type = 'button',
   color = 'black',
   onClick = () => {},
 }) => {
-  const [iconState, setIconState] = useState('down');
-
   const handleButtonClick = () => {
-    setIconState((prevState) => (prevState === 'down' ? 'up' : 'down'));
     onClick();
   };
 
@@ -66,28 +57,17 @@ const Button = ({
     colorClasses[color],
   ].join(' ');
 
-  const leftIconSrc = leftIcon ? 'public/images/Filter-icon.png' : null;
-  const rightIconSrc = rightIcon && `public/images/${iconState === 'down' ? 'up' : 'down'}.png`;
-
   return (
     <button type={type} className={buttonClasses} onClick={handleButtonClick}>
-      {leftIcon && <img src={leftIconSrc} alt="left_icon" className="w-5" />}
       {children}
-      {image && <img src={image} alt="button_image" className="w-full h-full" />}
-      {rightIcon && (
-        <img src={rightIconSrc} alt="right_icon" className="w-4" />
-      )}
     </button>
   );
 };
 
 Button.propTypes = {
-  children: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
   variant: PropTypes.oneOf(['primary', 'secondary', 'outline']),
   size: PropTypes.oneOf(['small', 'medium', 'large']),
-  rightIcon: PropTypes.string,
-  leftIcon: PropTypes.string,
-  image: PropTypes.string,
   type: PropTypes.oneOf(['button', 'submit', 'reset']),
   color: PropTypes.oneOf(['green', 'blue', 'red', 'gray']),
   onClick: PropTypes.func,
