b3ba6ff30b1d151f3617774b18454901ba101ce2
Dương Thế Hiển <duongthehien2001@gmail.com>
2023-07-14 17:07:07 +0700

    fix: rule eslint

--- practice-two/.eslintrc.cjs
+++ practice-two/.eslintrc.cjs
@@ -37,5 +37,8 @@ module.exports = {
     'linebreak-style': 0,
     'react/require-default-props': 0, // Turn Off define propDefault
     'react/function-component-definition': 0, // Using Class const
+    'react/button-has-type': 'error', // Add rule to require button elements to have a type attribute
+    'no-console': 'warn', // Add rule to warn when using console.log() and similar methods
+    'no-unused-vars': 'error', // Add rule to error when variables are declared but not used
   },
 };
--- practice-two/src/App.jsx
+++ practice-two/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from './components/common/Button';
 
-const App = () => {
+const App = () => (
   <>
     <div className=" flex gap-6">
       <Button variant="primary">
@@ -76,7 +76,7 @@ const App = () => {
         />
       </Button>
     </div>
-  </>;
-};
+  </>
+);
 
 export default App;
