e1ee0b8ceecd86230f3bee94dfe4c439137b72a7
Dương Thế Hiển <duongthehien2001@gmail.com>
2023-07-18 16:19:38 +0700

    #51 fix: update comment

--- practice-two/src/components/common/Popover.jsx
+++ practice-two/src/components/common/Popover.jsx
@@ -11,33 +11,24 @@ import Button from './Button';
  * @param closeButton - Flag to determine whether to show the close button.
  * @returns {JSX.Element|null} The Popover component JSX.
  */
-const Popover = ({ isOpen, onClose, closeButton = true, children }) => {
-  // CSS styles for the popover (using Tailwind CSS classes)
-  // prettier-ignore
-  const popoverStyles = 'popover absolute flex top-10 -left-5 bg-white border-2 border-gray-300 m-4 p-5 z-10';
-  const popoverInnerStyles = 'popover-inner relative flex';
-  const popoverMainStyles = 'popover-main flex';
-  const popoverContentStyles = 'popover-content';
-  const popoverClosesStyles = 'popover-closes';
-
-  return (
-    isOpen && (
-      <div className={popoverStyles}>
-        <div className={popoverInnerStyles}>
-          <div className={popoverMainStyles}>
-            <div className={popoverContentStyles}>{children}</div>
-            {/* Render the close button only if closeButton prop is true */}
-            {closeButton && (
-              <div className={popoverClosesStyles}>
-                <Button onClick={onClose}>Close</Button>
-              </div>
-            )}
-          </div>
+// prettier-ignore
+const Popover = ({ isOpen, onClose, closeButton = true, children }) => (
+  isOpen ? (
+    <div className="popover absolute flex top-10 -left-5 bg-white border-2 border-gray-300 m-4 p-5 z-10">
+      <div className="popover-inner relative flex">
+        <div className="popover-main flex">
+          <div className="popover-content">{children}</div>
+          {/* Render the close button only if closeButton prop is true */}
+          {closeButton && (
+            <div className="btn-close">
+              <Button onClick={onClose}>Close</Button>
+            </div>
+          )}
         </div>
       </div>
-    )
-  );
-};
+    </div>
+  ) : null
+);
 
 Popover.propTypes = {
   isOpen: PropTypes.bool.isRequired,
