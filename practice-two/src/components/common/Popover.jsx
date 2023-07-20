6ebd3fd90b95f62f2cb79b1a8ef1c53467f60b10
Dương Thế Hiển <duongthehien2001@gmail.com>
2023-07-20 16:35:03 +0700

    #53 style: update sytle for arrow-filter

--- practice-two/src/components/common/Popover.jsx
+++ practice-two/src/components/common/Popover.jsx
@@ -14,7 +14,7 @@ import Button from './Button';
 // prettier-ignore
 const Popover = ({ isOpen, onClose, closeButton = true, children }) => (
   isOpen ? (
-    <div className="absolute flex top-10 -left-5 bg-white border-2 border-gray-300 m-4 z-10">
+    <div className="absolute flex top-10 -left-5 bg-white m-4 z-200 shadow-2xl">
       <div className="flex">
         {children}
         {/* Render the close button only if closeButton prop is true */}
@@ -24,6 +24,7 @@ const Popover = ({ isOpen, onClose, closeButton = true, children }) => (
           </div>
         )}
       </div>
+      <div className="h-8 w-8 rounded border rotate-45 bg-transparent border-gray-300 shadow-2xl absolute left-6 top-[-6px] -z-10" />
     </div>
   ) : null
 );
