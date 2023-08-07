8b00dd3875527d387f67c5b8893ed99ea47d82c0
TheHienDuong <duongthehien2001@gmail.com>
2023-08-07 10:11:52 +0700

    #75 feat: update comment

--- practice-two/src/components/ProductDetails.jsx
+++ practice-two/src/components/ProductDetails.jsx
@@ -19,13 +19,15 @@ import FavoriteButton from './common/Button/ButtonFavorite';
 const ProductDetails = (products = [], isFavoriteProduct = []) => {
   const product = products.products;
   return (
-    <div className="container min-h-screen flex justify-center items-center">
+    <section className="container min-h-screen flex justify-center items-center">
       <div className="box bg-white rounded-2xl shadow-2xl p-10 m-2 w-96 grid grid-cols-1 lg:grid-cols-2 grid-rows-auto md:w-3/4 md:grid-rows-auto items-center">
         {/* Images list */}
         <div className="grid grid-cols-3 grid-rows-auto gap-3 mr-5">
+          {/* Displaying multiple images */}
           <div className="border rounded-2xl active col-span-3">
             <CardImage src={product.image} alt="This is a picture of the product" />
           </div>
+          {/* Additional images */}
           <div className="border rounded-2xl">
             <CardImage src={product.image} alt="This is a picture of the product" />
           </div>
@@ -39,10 +41,12 @@ const ProductDetails = (products = [], isFavoriteProduct = []) => {
         <div>
           <div className="basic-info flex flex-col gap-4 relative">
             <div className="flex justify-between">
+              {/* Displaying product name and favorite button */}
               <Typography level={3} className="text-4xl" size="xl">
                 {product.name}
               </Typography>
               <div className="flex justify-between">
+                {/* Displaying installment info (if available) */}
                 {products.installment && (
                   <div className="absolute top-2 left-2">
                     <Typography color="black" size="xl" variant="solid">
@@ -50,9 +54,11 @@ const ProductDetails = (products = [], isFavoriteProduct = []) => {
                     </Typography>
                   </div>
                 )}
+                {/* Displaying the favorite button */}
                 <FavoriteButton isFavorite={isFavoriteProduct} />
               </div>
             </div>
+            {/* Displaying product version and resolution */}
             <div className="card-compare flex gap-2">
               <Typography color="black" size="md" variant="solid">
                 {product.version}
@@ -61,15 +67,18 @@ const ProductDetails = (products = [], isFavoriteProduct = []) => {
                 {product.resolution}
               </Typography>
             </div>
+            {/* Displaying product price */}
             <Typography size="xl" color="red" variant="plain">
               {product.price}
               &#8363;
             </Typography>
+            {/* Displaying product description */}
             <Typography className="leading-6" color="gray-300" size="md" variant="plain">
               {product.description}
             </Typography>
+            {/* Buy button */}
             <Link
-              href="/products-card-ddetails"
+              href="/products-card-details"
               variant="bg-secondary py-2 px-4 rounded font-semibold text-sm"
               disabled
               color="white"
@@ -81,7 +90,7 @@ const ProductDetails = (products = [], isFavoriteProduct = []) => {
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
