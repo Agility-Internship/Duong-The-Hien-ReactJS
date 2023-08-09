47f83274e19d1c0e7240389757021d513cf9442d
TheHienDuong <duongthehien2001@gmail.com>
2023-08-09 08:55:26 +0700

    feat: update reponsive

--- practice-two/src/components/ProductDetails.jsx
+++ practice-two/src/components/ProductDetails.jsx
@@ -71,7 +71,7 @@ const ProductDetails = ({
             <div>
               <div className="basic-info flex flex-col gap-4 relative mt-5 ">
                 <div className="flex justify-between hover:text-blue-700 lg:w-[90%] ">
-                  <Typography level={3} size="xl" className="text-4xl">
+                  <Typography level={3} size="xl" className="text-3xl">
                     {selectedProduct.name}
                   </Typography>
                   <FavoriteButton
