ebab9ea777382f65904c42493cf9a9c68cc1e9fe
TheHienDuong <duongthehien2001@gmail.com>
2023-08-17 09:48:13 +0700

    #92 fix: fix comment

--- practice-two/src/components/SliderBar.jsx
+++ practice-two/src/components/SliderBar.jsx
@@ -11,9 +11,9 @@ import Button from './common/Button/Button';
  * @param closeButton - Flag to determine whether to show the close button.
  * @param closeButtonContent - Text content for the close button.
  * @param closeButtonStyle - CSS class for styling the close button.
- * @param sliderBarClassName - Custom class name for the main SliderBar div.
+ * @param customSliderBarClassNames - Custom class name for the main SliderBar div.
  * @param isFixed - Flag to determine whether to use fixed positioning for the SliderBar.
- * @param customClassNames - Custom class names to be added to elements.
+ * @param customBackdropClassNames - Custom class names to be added to elements.
  * @returns {JSX.Element|null} The SliderBar component JSX.
  */
 const SliderBar = ({
@@ -23,27 +23,26 @@ const SliderBar = ({
   closeButtonContent = 'Close',
   closeButtonStyle = '',
   children,
-  sliderBarClassName,
   isFixed = false,
-  customClassNames, // Changed from fixedOverlayClassName
+  customBackdropClassNames,
+  customSliderBarClassNames,
 }) => {
   const defaultSliderBarClassName = 'absolute flex top-10 -left-5 bg-white m-4 z-200 shadow-2xl z-50';
-  const customSliderBarClassName = sliderBarClassName
-    ? `${sliderBarClassName}`
+  const customSliderBarClassName = customSliderBarClassNames
+    ? `${customSliderBarClassNames}`
     : defaultSliderBarClassName;
 
   return isOpen ? (
     <>
       {isFixed && (
         <button
-          className={`fixed inset-0 bg-opacity-70 backdrop-filter backdrop-blur-lg flex items-center justify-center bg-gray-800 ${customClassNames}`}
+          className={`fixed inset-0 bg-opacity-70 backdrop-filter backdrop-blur-lg flex items-center justify-center ${customBackdropClassNames}`}
           onClick={onCloseSliderBar}
           onKeyDown={onCloseSliderBar}
-        >
-          {/* Content goes here */}
-        </button>
+          aria-label="Close Slider Bar"
+        />
       )}
-      <div className="z-30">
+      <aside className="z-30">
         <div className={customSliderBarClassName}>
           <div className="flex">
             {children}
@@ -56,7 +55,7 @@ const SliderBar = ({
             )}
           </div>
         </div>
-      </div>
+      </aside>
     </>
   ) : null;
 };
@@ -67,9 +66,9 @@ SliderBar.propTypes = {
   closeButton: PropTypes.bool,
   closeButtonContent: PropTypes.string,
   closeButtonStyle: PropTypes.string,
-  sliderBarClassName: PropTypes.string,
+  customSliderBarClassNames: PropTypes.string,
   isFixed: PropTypes.bool,
-  customClassNames: PropTypes.string, // Changed from fixedOverlayClassName
+  customBackdropClassNames: PropTypes.string,
   children: PropTypes.node,
 };
 
