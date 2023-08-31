87150152ccd1504303188f41ee9b3789613aae27
TheHienDuong <duongthehien2001@gmail.com>
2023-08-31 11:09:06 +0700

    feat: udpate theme

--- practice-two/src/components/TotalSection.jsx
+++ practice-two/src/components/TotalSection.jsx
@@ -20,22 +20,22 @@ const TotalSection = ({ productPrices = {} }) => {
 
   return (
     <div className="py-4 text-sm text-neutral-500  dark:text-neutral-400">
-      <div className="mb-3 flex items-center justify-between border-b border-neutral-200 pb-1 dark:border-neutral-700">
+      <div className="mb-3 flex items-center justify-between border-b border-neutral-200 pb-1 dark:border-neutral-400">
         <Typography variant="plain">Taxes</Typography>
-        <Typography variant="plain" className="text-right text-base text-black dark:text-white">
+        <Typography variant="plain" className="text-right text-base text-black ">
           0.00
           <span className="ml-1 inline">VNĐ</span>
         </Typography>
       </div>
-      <div className="mb-3 flex items-center justify-between border-b border-neutral-200 pb-1 pt-1 dark:border-neutral-700">
+      <div className="mb-3 flex items-center justify-between border-b border-neutral-200 pb-1 pt-1 dark:border-neutral-400">
         <Typography variant="plain">Shipping</Typography>
         <Typography variant="plain" className="text-right">
           Calculated at checkout
         </Typography>
       </div>
-      <div className="mb-3 flex items-center justify-between border-b border-neutral-200 pb-1 pt-1 dark:border-neutral-700">
+      <div className="mb-3 flex items-center justify-between border-b border-neutral-200 pb-1 pt-1 dark:border-neutral-400">
         <Typography variant="plain">Total</Typography>
-        <Typography variant="plain" className="text-right text-base text-black dark:text-white">
+        <Typography variant="plain" className="text-right text-base text-black">
           {formatCurrency(totalPrice)}
           <span className="ml-1 inline">VNĐ</span>
         </Typography>
